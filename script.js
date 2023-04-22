a33cb83669a14217a4f1ba3ba0202d973da8e28e
Jorge Cardenas <jorgecardenas11a@gmail.com>
2023-04-21 20:57:40 -0500

    modifico javascript

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 const octokit = new Octokit({
   auth: 'YOUR-TOKEN'
 })
-
 await octokit.request('GET /repos/{owner}/{repo}/pages', {
   owner: 'OWNER',
   repo: 'REPO',
   headers: {
     'X-GitHub-Api-Version': '2022-11-28'
-  }
-})
-
-
+        }
+    }
+)      
 function encript(){   
     let texto = document.querySelector('.areaEscrita').value.toLowerCase()
     let textEncriptado = texto.replace(/e/g,'enter')
@@ -18,7 +16,6 @@ function encript(){
     textEncriptado= textEncriptado.replace(/a/g,'ai')
     textEncriptado= textEncriptado.replace(/o/g,'ober')
     textEncriptado= textEncriptado.replace(/u/g,'ufat')  
-
     document.querySelector('.copy').style.display ="show"
     document.querySelector('.copy').style.display ="inherit"
     document.querySelector('.muneco').style.display ="none"
@@ -26,8 +23,6 @@ function encript(){
     document.querySelector('.p2').innerHTML = textEncriptado
     alinear()
 }
-
-
 function desencript(){
     let text = document.querySelector('.areaEscrita').value.toLowerCase()
     let textDesencriptado = text.replace(/enter/igm,'e')
@@ -35,16 +30,12 @@ function desencript(){
     textDesencriptado = textDesencriptado.replace(/ai/igm,'a')
     textDesencriptado = textDesencriptado.replace(/ober/igm,'o')
     textDesencriptado = textDesencriptado.replace(/ufat/igm,'u')
-    
     document.querySelector('.copy').style.display ="show"
     document.querySelector('.copy').style.display ="inherit"
     document.querySelector('.muneco').style.display ="none"
     document.querySelector('.p1').style.display = 'none'
     document.querySelector('.p2').innerHTML= textDesencriptado  
 }
-
-
-
 const btnCopiar = document.querySelector('.copy')
     btnCopiar.addEventListener('click',()=>{
     var content = document.querySelector('.p2').textContent
@@ -52,20 +43,15 @@ const btnCopiar = document.querySelector('.copy')
     console.log('texto copiado ' + content)
     btnCopiar.textContent = 'Copiado'
 })
-
 function copear(){
     var contenido = tes
     navigator.clipboard.writeText(contenido)
     alert('texto copiado '+ contenido)
-
 }
-
 function alinear(){
     let texto = document.querySelector('.p2')
     texto.style.color = '#0A3871'
     texto.style.fontSize = '30px'
     texto.style.top = "50px";
     texto.style.left= '1px'
-
 }
-
